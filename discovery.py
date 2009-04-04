f8a48191cb3de075ed6cbe25227e1aad18d31e44
fuzzyman <fuzzyman@localhost>
2009-04-04 22:33:13 +0000

    Simplify by using a TestLoader

--- discovery.py
+++ discovery.py
@@ -1,42 +1,9 @@
 import os
 import sys
-from unittest import TestCase, makeSuite, TextTestRunner, TestSuite
+from unittest import TestCase, TextTestRunner, TestLoader
 from types import ModuleType
 from fnmatch import fnmatch
 
-
-# doesn't allow you to specify a custom loader or runner
-# doesn't handle __path__ for test packages that extend themselves in odd ways
-# all tests must be in valid packages and importable from the top level of the project
-# filters for test names on the wish list
-# command line usage 'needs work'...
-
-
-def is_test_case(test):
-    if test is TestCase:
-        return False
-    if type(test) == type and issubclass(test, TestCase):
-        # skip TestCase subclasses that don't have tests on them
-        tests = [entry for entry in dir(test) if entry.startswith('test')]
-        return bool(tests)
-    return False
-    
-def AddTests(suite, test):
-    if isinstance(test, ModuleType):
-        for entry in test.__dict__.values():
-            if is_test_case(entry):
-                suite.addTests(makeSuite(entry))
-    elif is_test_case(test):
-        suite.addTests(makeSuite(test))
-
-def MakeSuite(tests):
-    suite = TestSuite()
-    for test in tests:
-        AddTests(suite, test)
-    return suite
-
-def run_tests(suite, **keywargs):
-    return TextTestRunner(**keywargs).run(suite)
 
 def find_files(start_dir, include_filter, exclude_filter, top_level):
     paths = os.listdir(start_dir)
@@ -53,20 +20,20 @@ def find_files(start_dir, include_filter, exclude_filter, top_level):
             for entry in find_files(full_path, include_filter, exclude_filter, False):
                 yield entry
 
+                
 def make_suite_from_files(test_paths, top_level_dir):
-    modules = []
+    names = []
     for path in test_paths:
         if not path.endswith('.py'):
             continue
         path = os.path.normpath(path)[:-3]
         
         # we don't handle drive / volume names
-        module_name = os.path.relpath(path,top_level_dir).replace(os.path.sep, '.')
+        name = os.path.relpath(path, top_level_dir).replace(os.path.sep, '.')
+        names.append(name)
         
-        __import__(module_name)
-        mod = sys.modules[module_name]
-        modules.append(mod)
-    return MakeSuite(modules)
+    return TestLoader().loadTestsFromNames(names)
+
 
 def run(start_dir='.', include_filter='test*.py', exclude_filter=None, 
         top_level_dir=None, **kwargs):
@@ -82,8 +49,18 @@ def run(start_dir='.', include_filter='test*.py', exclude_filter=None,
     test_paths = find_files(start_dir, include_filter, exclude_filter, is_top_level)
         
     suite = make_suite_from_files(test_paths, top_level_dir)
-    run_tests(suite, **kwargs)
     
+    # need to return exit code here
+    TextTestRunner(**kwargs).run(suite)
 
+    
 if __name__ == '__main__':
     run(*sys.argv[1:])
+    
+# uses os.path.relpath so requires Python 2.6+
+# doesn't allow you to specify a custom loader or runner
+# doesn't handle __path__ for test packages that extend themselves in odd ways
+# all tests must be in valid packages and importable from the top level of the project
+# filters for test names on the wish list
+# command line usage 'needs work'...
+
