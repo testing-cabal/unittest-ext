fe4ba419646105086587d505789de949061331b3
fuzzyman <fuzzyman@localhost>
2010-02-12 17:12:13 +0000

    Adding a setup.cfg to force a new MANIFEST when building packages.

--- unittest2/unittest2/__init__.py
+++ unittest2/unittest2/__init__.py
@@ -31,7 +31,7 @@ __all__ = ['TestResult', 'TestCase', 'TestSuite',
            'defaultTestLoader', 'SkipTest', 'skip', 'skipIf', 'skipUnless',
            'expectedFailure', 'TextTestResult', '__version__']
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 # Expose obsolete functions for backwards compatibility
 __all__.extend(['getTestCaseNames', 'makeSuite', 'findTestCases'])
