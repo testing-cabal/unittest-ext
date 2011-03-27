cf8887f15537de614871a6a29003702da45aae4f
Mark Roddy <markroddy@gmail.com>
2011-03-26 21:42:08 -0400

    Added the ability to specify the branch and revision within the mercurial repository to use as the base line for generating the library.  These values are stored in hg-python.ini.  Also added enter_dir() context manager to remove the need to always go back and forth between the mercurial repo.

--- unittest2py3k/fabfile.py
+++ unittest2py3k/fabfile.py
@@ -1,29 +1,49 @@
 
 import os
 import shutil
+import ConfigParser
 from fabric.operations import local
+from contextlib import contextmanager
 
 PYTHON_HG_URL = 'http://hg.python.org/cpython/'
-REVISION_FILE = "default-revision.txt"
+CPYTHON_INI = "hg-cpython.ini"
+
+@contextmanager
+def enter_dir(dirname):
+    orig_dir = os.getcwd()
+    os.chdir(dirname)
+    yield
+    os.chdir(orig_dir)
+
 
 def create_lib(rev = None):
     """Create the backwards compatible unittest2 library"""
-    if not rev:
-        with open(REVISION_FILE) as f:
-            rev = f.read().strip()
+
+    # Get or update the cpython hg repo
     if os.path.exists("cpython"):
-        os.chdir("cpython")
-        local("hg pull")
-        local("hg update")
-        os.chdir(os.pardir)
+        with enter_dir("cpython"):
+            # local("hg pull")
+            pass
+            # local("hg update") Delete?
     else:
         local("hg clone %s" % PYTHON_HG_URL)
 
+    # Switch to the desired branch/revision
+    cfg = ConfigParser.ConfigParser()
+    cfg.read(CPYTHON_INI)
+    branch = cfg.get('default', 'branch')
+    rev = cfg.get('default', 'revision')
+
+    with enter_dir("cpython"):
+        local("hg update -C %s" % branch)
+        local("hg revert --no-backup -r %s Lib/unittest" % rev)
+            
+    # Remove the old copy of lib if exists
     if os.path.exists("unittest2"):
         local("rm -rf unittest2")
     shutil.copytree('cpython/Lib/unittest', 'unittest2')
     local("touch unittest2/compatibility.py")
-    local("cat unittest2-py3k.patch|patch -p0")
+    local("cat unittest2-py3k.patch|patch -p0 --no-backup-if-mismatch")
 
 
 def update_patch():
